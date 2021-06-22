d00e9a5282d4ff85816d60caa010c3afe730fd05
Bima Pramudya Asaddulloh <pram.bima@gmail.com>
2021-06-22 11:10:03 +0700

    initial commit

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -21,6 +21,7 @@ class Barang extends CI_Controller
 			<td>Nama</td>
 			<td>Deskripsi</td>
 			<td>Stok</td>
+			<td>Foto</td>
 			<td>Aksi</td>
 		</tr>';
 
@@ -29,9 +30,11 @@ class Barang extends CI_Controller
 							<td>'.$value->nama_barang.'</td>
 							<td>'.$value->deskripsi.'</td>
 							<td>'.$value->stok.'</td>
-							<td>Read | Hapus | <a href="#'.$value->id_barang.'" class="linkEditBarang">Edit</a></td>
+							<td><img src="'.base_url().'/foto/'.$value->id_barang.'/'.$value->foto_produk.'" width="50"></td>
+							<td>Read | <a href="#'.$value->id_barang.'" class="linkHapusBarang">Hapus</a> | <a href="#'.$value->id_barang.'" class="linkEditBarang">Edit</a></td>
 						</tr>';
 		}
+
 
 		$data_json = array(
 			'konten' => $konten,
@@ -50,6 +53,12 @@ class Barang extends CI_Controller
 		);
 
 		$this->Barang_model->insert_data($arr_input);
+
+		$id_barang = $this->db->insert_id();
+
+		if ($_FILES != null) {
+			$this->upload_foto($id_barang, $_FILES);
+		}
 
 		if ($this->db->trans_status() === FALSE) {
 			$this->db->trans_rollback();
@@ -82,11 +91,15 @@ class Barang extends CI_Controller
 		$id_barang = $this->input->post('id_barang');
 
 		$arr_input = array(
-			'nama_barang' -> $this->input->post('nama_barang'),
+			'nama_barang' => $this->input->post('nama_barang'),
 			'deskripsi' => $this->input->post('deskripsi'),
 		);
 
 		$this->Barang_model->update_data($id_barang, $arr_input);
+
+		if ($_FILES != null) {
+			$this->upload_foto($id_barang, $_FILES);
+		}
 
 		if ($this->db->trans_status() === FALSE) {
 			$this->db->trans_rollback();
@@ -97,5 +110,106 @@ class Barang extends CI_Controller
 		}
 
 		echo json_encode($data_output);
+	}
+
+	public function delete_data() {
+		$this->db->trans_start();
+
+		$id_barang = $this->input->get('id_barang');
+
+		$this->Barang_model->hapus_data($id_barang);
+
+		if ($this->db->trans_status() === FALSE) {
+			$this->db->trans_rollback();
+			$data_output = array('sukses' => 'tidak', 'pesan' => 'Gagal Hapus Data Barang');
+		} else {
+			$this->db->trans_commit();
+			$data_output = array('sukses' => 'ya', 'pesan' => 'Berhasil Hapus Data Barang');
+		}
+
+		echo json_encode($data_output);
+	}
+
+	public function soft_delete_data() {
+		$this->db->trans_start();
+
+		$id_barang = $this->input->get('id_barang');
+
+		$this->Barang_model->soft_delete_data($id_barang);
+
+		if ($this->db->trans_status() === FALSE) {
+			$this->db->trans_rollback();
+			$data_output = array('sukses' => 'tidak', 'pesan' => 'Gagal Hapus Data Barang');
+		} else {
+			$this->db->trans_commit();
+			$data_output = array('sukses' => 'ya', 'pesan' => 'Berhasil Hapus Data Barang');
+		}
+
+		echo json_encode($data_output);
+	}
+
+	public function cari_barang() {
+		$cari_nama = $this->input->post('cari_nama');
+		$cari_deskripsi = $this->input->post('cari_desk');
+		$cari_stok = $this->input->post('cari_stok');
+
+		$data_barang = $this->Barang_model->get_barang($cari_nama, $cari_deskripsi, $cari_stok);
+
+		$konten = '<tr>
+			<td>Nama</td>
+			<td>Deskripsi</td>
+			<td>Stok</td>
+			<td>Aksi</td>
+		</tr>';
+
+		foreach ($data_barang->result() as $key => $value) {
+			$konten .= '<tr>
+							<td>'.$value->nama_barang.'</td>
+							<td>'.$value->deskripsi.'</td>
+							<td>'.$value->stok.'</td>
+							<td>Read | <a href="#'.$value->id_barang.'" class="linkHapusBarang">Hapus</a> | <a href="#'.$value->id_barang.'" class="linkEditBarang">Edit</a></td>
+						</tr>';
+		}
+
+		$data_json = array(
+			'konten' => $konten,
+		);
+
+		echo json_encode($data_json);
+	}
+
+	private function upload_foto($id_barang, $files) {
+		$gallerPath = realpath(APPATH.'../foto');
+		$path = $gallerPath.'/'.$id_barang;
+
+		if (!is_dir($path)) {
+			mkdir($path, 0777, TRUE);
+		}
+
+		$konfigurasi = array(
+			'allowed_types' => 'jpg|png|jpeg',
+			'upload_path' => $path,
+			'overwrite' => true
+		);
+
+		$this->load->library('upload', $konfigurasi);
+
+		$_FILES['file']['name'] = $files['file']['name'];
+		$_FILES['file']['type'] = $files['file']['type'];
+		$_FILES['file']['tmp_name'] = $files['file']['tmp_name'];
+		$_FILES['file']['error'] = $files['file']['error'];
+		$_FILES['file']['size'] = $files['file']['size'];
+
+		if ($this->upload->do_upload('file')) {
+			$data_barang = array(
+				'foto_produk' => $this->upload->data('file_name')
+			);
+
+			$this->Barang_model->update_data($id_barang, $data_barang);
+
+			return 'Success Upload';
+		} else {
+			return 'Error Upload';
+		}
 	}
 }
--- application/models/Barang_model.php
+++ application/models/Barang_model.php
@@ -7,10 +7,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Barang_model extends CI_Model
 {
 	
-	public function get_barang()
-	{
+	public function get_barang($cari_nama = '', $cari_deskripsi = '', $cari_stok = '') {
 		$this->db->select('*');
 		$this->db->from('barang');
+		$this->db->where('status_delete', 0);
+
+		if ($cari_nama != '' && $cari_nama != null) {
+			$this->db->like('nama_barang', $cari_nama);
+		}
+
+		if ($cari_deskripsi != '' && $cari_deskripsi != null) {
+			$this->db->like('deskripsi', $cari_deskripsi);
+		}
+
+		if ($cari_stok != '' && $cari_stok != null) {
+			$this->db->where('stok <=', $cari_stok);
+		}
+
 		return $this->db->get();
 	}
 
@@ -26,6 +39,19 @@ class Barang_model extends CI_Model
 	}
 
 	public function update_data($id_barang, $data) {
+		$this->db->where('id_barang', $id_barang);
+		$this->db->update('barang', $data);
+	}
+
+	public function hapus_data($id_barang) {
+		$this->db->where('id_barang', $id_barang);
+		$this->db->delete('barang');
+	}
+
+	public function soft_delete_data($id_barang) {
+		$data = array(
+			'status_delete' => 1
+		);
 		$this->db->where('id_barang', $id_barang);
 		$this->db->update('barang', $data);
 	}
